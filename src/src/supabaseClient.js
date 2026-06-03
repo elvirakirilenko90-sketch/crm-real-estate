3075e824f10b849ee878dbf86ed2fb48c5685da2
elvirakirilenko90-sketch <elvirakirilenko90@gmail.com>
2026-06-03 23:38:19 +0300

    Delete src/src directory

--- src/src/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://stvinrjyfuskqddcxzjv.supabase.co";
-const supabaseKey = "sb_publishable_MA2mF2cPre1gGnfUfjhKuA_BsD61g5A";
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-);
